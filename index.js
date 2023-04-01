e299965d4e53470a1d6c3f6418150bbf103e26ac
Leonardo Reyes <reyesleonardo44@yahoo.com>
2023-03-31 21:10:24 -0700

    Control flow lab complete

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideLength){
   // Write your code here!
+  if (rideLength <= 400) {
+    return "This one is on me!";
+  }
+  else if (rideLength > 400 && rideLength <= 2000) {
+    return "That will be twenty bucks.";
+  }
+  else if (rideLength > 2000 && rideLength < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(anyGivenCity){
   // Write your code here!
+  let response = anyGivenCity === "NYC" ? ("Ok, sounds good.") : ("No go.");
+  return response;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(theirResponse){
   // Write your code here!
+  let myResponse;
+  switch (theirResponse) {
+    case "generous":
+      myResponse = "Thank you so much.";
+      break;
+    case "not as generous":
+      myResponse = "Thank you.";
+      break;
+    default:
+      myResponse = "Bye.";
+      break;
+  }
+  return myResponse;
+
+
 }
